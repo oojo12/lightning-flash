17845495c437e8b39d69ff305080447af16dcdad
oojo12 <47154698+oojo12@users.noreply.github.com>
2022-02-14 13:17:26 -0500

    Create unet_segmentation.sh

---
+++ grid_examples/unet_segmentation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# convert to multi-gpu training by selectiing an instance with multiple gpus from https://docs.grid.ai/products/runs/machines and incrementing the gpus flag
+grid run --name unet --instance_type=g4dn.xlarge --gpus=1
