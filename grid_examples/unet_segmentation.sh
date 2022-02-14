da2af0ff9421cf1561d0d036df08cacd02a89d67
oojo12 <47154698+oojo12@users.noreply.github.com>
2022-02-14 13:30:04 -0500

    Update unet_segmentation.sh

--- grid_examples/unet_segmentation.sh
+++ grid_examples/unet_segmentation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # convert to multi-gpu training by selectiing an instance with multiple gpus from https://docs.grid.ai/products/runs/machines and incrementing the gpus flag
-grid run --name unet --instance_type=g4dn.xlarge --gpus=1
+grid run --name unet --instance_type=g4dn.xlarge --gpus=1 ../flash_examples/semantic_segmentation_unet.py
