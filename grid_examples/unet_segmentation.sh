9aa91eb1e07989912cc1c5529fa2c1d3d83c2586
oojo12 <47154698+oojo12@users.noreply.github.com>
2022-02-22 14:44:01 -0500

    Update unet_segmentation.sh

--- grid_examples/unet_segmentation.sh
+++ grid_examples/unet_segmentation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # convert to multi-gpu training by selectiing an instance with multiple gpus from https://docs.grid.ai/products/runs/machines and incrementing the gpus flag
-grid run --name unet --dependency_file ./requirements.txt --instance_type=g4dn.xlarge --gpus=1 ../flash_examples/semantic_segmentation_unet.py
+grid run --name unet --instance_type=g4dn.xlarge --gpus=1 ../flash_examples/semantic_segmentation_unet.py
 
 # multi-gpu example
-grid run --name unet-multi-gpu --dependency_file ./requirements.txt --instance_type=g3.8xlarge --gpus=2 ../flash_examples/semantic_segmentation_unet.py
+grid run --name unet-multi-gpu --instance_type=g3.8xlarge --gpus=2 ../flash_examples/semantic_segmentation_unet.py
 
 # multi-gpu + HPO example
-grid run --name unet-multi-gpu-hpo --dependency_file ./requirements.txt --instance_type=g3.8xlarge --gpus=2 ../flash_examples/semantic_segmentation_unet.py --max_epochs="[32, 64, 128]"
+grid run --name unet-multi-gpu-hpo --instance_type=g3.8xlarge --gpus=2 ../flash_examples/semantic_segmentation_unet.py --max_epochs="[32, 64, 128]"
